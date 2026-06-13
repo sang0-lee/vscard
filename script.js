32ed599069f1ecc07ea1909deb9f9a4cceec2af5
이상영 <a27179820@gmail.com>
2026-06-13 22:29:48 +0900

    웹명함 작동1

--- script.js
+++ script.js
@@ -2,11 +2,36 @@ const card = document.querySelector(".card");
 
 card.addEventListener("mouseenter", () => {
     card.style.transform = "translateY(-10px)";
-    card.style.boxShadow = "0 25px 60px rgba(0, 0, 0, 0.18)";
-    card.style.transition = "0.3s ease";
+    card.style.boxShadow = "0 25px 60px rgba(176,21,249,.35)";
 });
 
 card.addEventListener("mouseleave", () => {
     card.style.transform = "translateY(0)";
-    card.style.boxShadow = "0 15px 40px rgba(0, 0, 0, 0.08)";
+    card.style.boxShadow = "0 15px 40px rgba(176,21,249,.2)";
+});
+
+const emailButton = document.getElementById("emailCopyButton");
+const emailText = document.getElementById("emailText");
+const copyMessage = document.getElementById("copyMessage");
+
+emailButton.addEventListener("click", async () => {
+
+    try {
+
+        await navigator.clipboard.writeText(
+            emailText.textContent.trim()
+        );
+
+        copyMessage.classList.add("show");
+
+        setTimeout(() => {
+            copyMessage.classList.remove("show");
+        }, 2000);
+
+    } catch (error) {
+
+        alert("이메일 복사 실패");
+
+    }
+
 });
