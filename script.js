46fd11fc5ca43af70560f6a4e5b9df7026109931
이상영 <a27179820@gmail.com>
2026-06-13 22:54:22 +0900

    웹명함 업글 2

--- script.js
+++ script.js
@@ -12,43 +12,40 @@ card.addEventListener("mouseleave", () => {
 
 const emailButton = document.getElementById("emailCopyButton");
 const emailText = document.getElementById("emailText");
+
+const phoneCopyButton = document.getElementById("phoneCopyButton");
+const phoneText = document.getElementById("phoneText");
+
 const copyMessage = document.getElementById("copyMessage");
-
-if (emailButton) {
-    emailButton.addEventListener("click", async () => {
-
-        try {
-
-            await navigator.clipboard.writeText(
-                emailText.textContent.trim()
-            );
-
-            copyMessage.classList.add("show");
-
-            setTimeout(() => {
-                copyMessage.classList.remove("show");
-            }, 2000);
-
-        } catch (error) {
-
-            alert("이메일 복사 실패");
-
-        }
-
-    });
-}
-
 const githubButton = document.getElementById("githubButton");
 
-if (githubButton) {
+emailButton.addEventListener("click", async () => {
+    try {
+        await navigator.clipboard.writeText(emailText.textContent.trim());
+        showCopyMessage("이메일이 복사되었습니다.");
+    } catch (error) {
+        alert("이메일 복사 실패");
+    }
+});
 
-    githubButton.addEventListener("click", () => {
+phoneCopyButton.addEventListener("click", async () => {
+    try {
+        await navigator.clipboard.writeText(phoneText.textContent.trim());
+        showCopyMessage("전화번호가 복사되었습니다.");
+    } catch (error) {
+        alert("전화번호 복사 실패");
+    }
+});
 
-        window.open(
-            "https://github.com/sang0-lee",
-            "_blank"
-        );
+githubButton.addEventListener("click", () => {
+    window.open("https://github.com/sang0-lee", "_blank");
+});
 
-    });
+function showCopyMessage(message) {
+    copyMessage.textContent = message;
+    copyMessage.classList.add("show");
 
+    setTimeout(() => {
+        copyMessage.classList.remove("show");
+    }, 2000);
 }
